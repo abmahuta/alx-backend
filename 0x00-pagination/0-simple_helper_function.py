37550111323bb897f3063ab98eafbeb8fe5120cd
abmahuta <mahuta2012@gmail.com>
2023-04-16 14:14:55 +0000

    error fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
+"""This module provides a function for calculating the start and end indices
+for a given page and page size, using 1-indexing
 """
-This module provides a function for calculating the start and end indices for
-a given page and page size, using 1-indexing.
-"""
-
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Calculates the start and end indices for the specified page and page size.
+    """Retrieves the index range from a given page and page size.
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
-
